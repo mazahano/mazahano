3d91627d86eb24397dc0e379318f723dde141aee
mazahano <mazahano7@gmail.com>
2026-02-09 18:06:42 +0530

    1

--- server/server.js
+++ server/server.js
@@ -17,7 +17,12 @@ if (!process.env.JWT_SECRET) {
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Connect to MongoDB
 // Connect to MongoDB
